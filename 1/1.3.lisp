485f4d70e94a1ce337c270f080992013cbe40106
Giorgi Koguashvili <kogo27@gmail.com>
2018-02-17 04:20:19 -0800

    Rename 1.2.lisp to 1.3.lisp

---
+++ 1/1.3.lisp
@@ -0,0 +1,10 @@
+(define (sum a b c)
+  (- (+ (square a)
+        (square b)
+        (square c))
+     (min a (min b c))))
+
+(define (square x) (* x x))
+(define (min a b) (if (< a b) a b))
+
+(sum 2 3 4)
