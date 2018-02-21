8d4306c1fad749d15c1450653d115121ff4f0f62
Giorgi Koguashvili <kogo27@gmail.com>
2018-02-21 14:19:19 +0400

    added sum-iter impl

--- 1/1.3.1. Процедуры в качестве аргументов.rkt
+++ 1/1.3.1. Процедуры в качестве аргументов.rkt
@@ -5,7 +5,7 @@
         0
         (+ a (sum-integers (+ a 1) b))))
 
-(sum-integers 1 10)
+(sum-integers 1 10) ;55
 
 (define (pi-sum a b)
     (define (f n)
@@ -14,6 +14,19 @@
         0
         (+ (f a) (pi-sum (+ a 4) b))))
 
-(define pi (* (pi-sum 1 9999999) 8.0))
-pi
+(define pi (* (pi-sum 1 99999) 8.0))
+pi ;3.141572653589795
+
+(define (sum-iter a b next fun)
+    (if (> a b)
+        0
+        (+ (fun a) (sum-iter (next a) b next fun))))
+
+(define (inc x) (+ x 1))
+(define (cube x) (* x x x))
+
+(define (sum-cubes a b)
+    (sum-iter a b inc cube))
+
+(sum-cubes 1 10) ;3025
 
