5eacabbee6a01e50205738d21083e5a2ddd3adfc
Giorgi Koguashvili <kogo27@gmail.com>
2018-02-22 16:58:24 +0400

    changed 1.31 solution

--- 1.32.rkt
+++ 1.32.rkt
@@ -5,18 +5,16 @@
         null-value
         (accumulate combiner (combiner null-value (term a)) term (next a) next b)))
 
+(define (inc a) (+ a 1))
+
 (define (sum a b)
-    (define (combiner prev res) (+ prev res))
     (define (identity a) a)
-    (define (inc a) (+ a 1))
-    (accumulate combiner 0 identity a inc b))
+    (accumulate + 0 identity a inc b))
 
 (sum 1 5);15
 
 (define (prod a b)
     (define (identity x) x)
-    (define (inc x) (+ x 1))
-    (define (combiner prev res) (* prev res))
-    (accumulate combiner 1 identity a inc b))
+    (accumulate * 1 identity a inc b))
 
 (prod 1 5); 120 
