02d96cb593eaa361059471b346f94ef0568e55ed
kogoia <kogo27@gmail.com>
2018-02-24 00:41:02 -0800

    added 1.2.6

---
+++ 1/1.2.6. Проверка на простоту.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(define (prime? n)
+    (= (smallest-divisor n) n))
+
+(define (smallest-divisor n)
+    (define (find-divisor n test-divisor)
+        (cond 
+            ((> (* test-divisor test-divisor) n) n)
+            ((divides? n test-divisor) test-divisor)
+            (else (find-divisor n (+ test-divisor 1)))))
+    (define (divides? n divisor) (= (remainder n divisor) 0))
+    (find-divisor n 2))
+
+
+(prime? 3)
+(prime? 5)
+(prime? 7)
+(prime? 13)
+(prime? 19)
+(prime? 10)
