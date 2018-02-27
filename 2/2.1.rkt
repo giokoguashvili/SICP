dadc70f127e023b5cea1ae955f0d60695d28d62c
kogoia <kogo27@gmail.com>
2018-02-27 02:18:16 -0800

    improved mul impl in 2.1

--- 2/2.1.rkt
+++ 2/2.1.rkt
@@ -1,10 +1,15 @@
 #lang racket
 
 (define (make-rat a b)
+    (abbreviated-rat (signed-rat (make-rat* a b))))
+
+(define (make-rat* a b)
     (lambda (selector)
         (selector a b)))
 
-(define (gcd a b)
+(define (gcd aa bb)
+    (define a (abs aa))
+    (define b (abs bb))
     (define (gcd* a b)
         (if (= b 0)
             a
@@ -17,7 +22,22 @@
     (define n (number x))
     (define d (denom x))
     (define g (gcd n d))
-    (make-rat (/ n g) (/ d g)))
+    (make-rat* (/ n g) (/ d g)))
+
+(define (abs x)
+        (if (< x 0)
+            (- x)
+            x))
+
+(define (signed-rat x)
+    (define n (number x))
+    (define d (denom x))
+    (define (sign x) 
+        (if (< x 0)
+            -1
+            1))
+    (make-rat* (abs n) (* (sign (* n d)) (abs d))))
+        
 
 (define (number x)
     (define (fst a b) a)
@@ -46,8 +66,8 @@
 
 (define (run)
     (let (
-            (one-half (make-rat 1 2))
-            (one-third (make-rat 1 3)))
+            (one-half (make-rat -1 2))
+            (one-third (make-rat -1 3)))
     (print-rat (add-rat one-half one-third))
     (print-rat (mul-rat one-half one-third))
     (print-rat (abbreviated-rat (add-rat one-third one-third)))))
