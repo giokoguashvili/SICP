ee4d74d6ec33571cbc55656c3e35d536aa5a13e0
Giorgi Koguashvili <kogo27@gmail.com>
2018-03-11 12:04:21 +0400

    added 2.25 solution

--- 2/2.24.rkt
+++ 2/2.24.rkt
@@ -47,3 +47,23 @@ xxx
 (leaves-count xxx)
 
 (cons 1 (cons 2 (cons 3 4))) 
+
+
+;;2.24
+;;(1 (2 (3 4)))
+
+;;2.25
+(display "# 2.25")
+(newline)
+;;(1 3 (5 7) 9)
+(define xs (list 1 2 (list 5 7) 9))
+;;((7))
+(define ys (list (list 7)))
+;;(1 (2 (3 (4 (5 (6 7))))))
+(define zs (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
+xs
+ys
+zs
+(car (cdr (car (cdr (cdr xs)))))
+(car (car ys))
+(car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr zs))))))))))))
