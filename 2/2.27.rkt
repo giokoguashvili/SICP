ee7cb00c472505d7293c4da984c73c6ea6dd6556
Giorgi Koguashvili <kogo27@gmail.com>
2018-03-24 22:33:45 +0400

    rewrited 2.27 solution

--- 2/2.27.rkt
+++ 2/2.27.rkt
@@ -1,19 +1,20 @@
 #lang racket
 
-(define xs (list (list 1 2 3) (list 1 2) (list 1)))
+(define xs (list (list 1 2 3) (list 4 5) (list 6)))
 xs
 (car xs)
+(cdr xs)
 
 (define (deep-reverse xs)
   (define (reverse-it ys)
     (if (list? ys)
       (deep-reverse ys)
       ys))
-  (define (deep-reverse-it src result)
-    (if (null? src)
-      result
-      (deep-reverse-it (cdr src)
-		       (cons (reverse-it (car src)) result))))
-  (deep-reverse-it xs (list)))
+  (define (deep-reverse-iter res ys)
+    (if (null? ys)
+      res
+      (deep-reverse-iter (cons (reverse-it (car ys)) res)
+			 (cdr ys))))
+  (deep-reverse-iter (list) xs))
 
 (deep-reverse xs)
